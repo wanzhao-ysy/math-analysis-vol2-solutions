7475bfa3b525585b3522b20d41fae9daedd0f379
wanzhao_ysy <wanzhao_ysy@qq.com>
2026-04-14 23:17:08 +0800

    feat: Significantly updated the file structure, adopting a mixed compilation approach using l3build and latexmk.

---
+++ build.lua
@@ -0,0 +1,14 @@
+module = "math-analysis-vol2-solutions"
+typesetfiles = { "main.tex" }
+supportdir = "."
+typesetruns = 1
+typesetsuppfiles = {
+    "chapters/**/*.tex",
+    "bib/*.bib",
+    "preambles/*.sty"
+}
+
+function typeset(file)
+    local cmd = "latexmk -lualatex -interaction=nonstopmode -file-line-error -shell-escape -synctex=1 " .. file
+    return runcmd(cmd, typesetdir)
+end
